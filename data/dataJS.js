2aa5ccdac09eda7498fb5f7ee25257d42e6f5bdd
Daniel Corredor Acosta <dann95.moz@gmail.com>
2022-01-12 20:40:17 -0500

    First commit

---
+++ data/dataJS.js
@@ -0,0 +1,19 @@
+export var Info =
+     [
+ {"id":1,"weight":"63","IMC":"21.80","age":"23","sex":"mujer","height":"1.7"},
+ {"id":2,"weight":"57","IMC":"22.27","age":"18","sex":"hombre","height":"1.6"},
+ {"id":3,"weight":"68","IMC":"23.53","age":"24","sex":"hombre","height":"1.7"},
+ {"id":4,"weight":"64","IMC":"19.75","age":"28","sex":"mujer","height":"1.8"},
+ {"id":5,"weight":"80","IMC":"22.16","age":"35","sex":"mujer","height":"1.90"},
+ {"id":6,"weight":"110","IMC":"33.95","age":"35","sex":"mujer","height":"1.80"},
+ {"id":7,"weight":"130","IMC":"44.98","age":"40","sex":"hombre","height":"1.70"},
+ {"id":8,"weight":"40","IMC":"13.84","age":"43","sex":"mujer","height":"1.70"},
+ {"id":9,"weight":"70","IMC":"29.14","age":"55","sex":"mujer","height":"1.55"},
+ {"id":10,"weight":"110","IMC":"40.40","age":"24","sex":"hombre","height":"1.65"},
+ {"id":11,"weight":"117","IMC":"36.11","age":"30","sex":"mujer","height":"1.8"},
+ {"id":12,"weight":"104","IMC":"31.40","age":"28","sex":"mujer","height":"1.82"},
+ {"id":13,"weight":"60","IMC":"25.30","age":"21","sex":"mujer","height":"1.54"},
+ {"id":14,"weight":"75","IMC":"24.49","age":"27","sex":"mujer","height":"1.75"},
+ {"id":15,"weight":"84","IMC":"29.41","age":"27","sex":"mujer","height":"1.69"}
+ ]
+ 
---
+++ main.js
@@ -0,0 +1,121 @@
+import {Info} from "./data/dataJS.js"
+console.log(Info)
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+var IMC
+
+btn.onclick = function() {
+  modal.style.display = "block";
+  var height= document.getElementById("height").value
+  var weight= document.getElementById("weight").value;
+  var age= document.getElementById("age").value;
+  var sex
+  var IMC=  weight/(height*height)
+  var fixed= IMC.toFixed(2)
+  document.getElementById("resultado").innerHTML=fixed
+
+  if (document.getElementById("man").src.match("/img/man.png") ){
+    sex= "mujer"
+  }
+  else {
+    sex="hombre"
+  }
+  if (fixed <18.5 ) {
+    var img = document.createElement("img");
+    img.src = "/img/arrow-up-s-fill.svg";
+    var div= document.getElementById("arrow-indicator")
+    div.style.marginLeft="38%"
+    div.appendChild(img)
+  }
+  if (fixed >= 18.5 && fixed<=24.9) {
+    var img = document.createElement("img");
+    img.src = "/img/arrow-up-s-fill.svg";
+    var div= document.getElementById("arrow-indicator")
+    div.style.marginLeft="46%"
+    div.appendChild(img)
+  }
+  if (fixed >=25 && fixed<=29.9) {
+    var img = document.createElement("img");
+    img.src = "/img/arrow-up-s-fill.svg";
+    var div= document.getElementById("arrow-indicator")
+    div.style.marginLeft="52%"
+    div.appendChild(img)
+  }
+  if (fixed >=30 && fixed<=39.9) {
+    var img = document.createElement("img");
+    img.src = "/img/arrow-up-s-fill.svg";
+    var div= document.getElementById("arrow-indicator")
+    div.style.marginLeft="56%"
+    div.appendChild(img)
+  }
+  if (fixed >40) {
+    var img = document.createElement("img");
+    img.src = "/img/arrow-up-s-fill.svg";
+    var div= document.getElementById("arrow-indicator")
+    div.style.marginLeft="61%"
+    div.appendChild(img)
+  }
+
+
+localStorage.setItem("height", height)
+localStorage.setItem("weight",weight)
+localStorage.setItem("age",age)
+localStorage.setItem("sex",sex)
+localStorage.setItem("IMC", fixed)
+var storedIMC= localStorage.getItem("IMC")
+var storedHeight = localStorage.getItem("height")
+var storedWeight = localStorage.getItem("weight")
+var storedAge = localStorage.getItem("age")
+var storedSex= localStorage.getItem("sex")
+console.log(storedHeight)
+console.log(storedWeight)
+console.log(storedAge)
+console.log(storedSex)
+console.log(storedIMC)
+
+
+
+
+}
+
+
+
+    
+  
+span.onclick = function() {
+  modal.style.display = "none";
+  document.location.reload();
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    document.location.reload();
+  }
+}
+
+
+
+function changeColorM(){ 
+    
+    if (document.getElementById("man").src.match("/img/man.png") ){
+        document.getElementById("man").src="img/picked-man.png"
+        document.getElementById("woman").src="img/woman.png"
+       
+    }
+    else{
+        document.getElementById("man").src= "/img/man.png"
+    }
+}
+function changeColorW(){
+   
+    if (document.getElementById("woman").src.match("/img/woman.png") ){
+        document.getElementById("woman").src="img/picked-woman.png"
+        document.getElementById("man").src="img/man.png"
+       
+    }
+    else{
+        document.getElementById("woman").src= "/img/woman.png"
+    }
+}
